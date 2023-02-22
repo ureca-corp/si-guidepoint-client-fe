65953c26faa2a3a623698f217d9be2b49159bbe6
schatzen <gomieti@naver.com>
2023-02-22 14:37:09 +0900

    Add: Layout setting

---
+++ src/common/components/CustomAppbar/CustomAppbar.tsx
@@ -0,0 +1,9 @@
+import { css } from "@emotion/react";
+
+export const CustomAppbar = () => {
+  return (
+    <div
+      style={{ width: "100%", height: "80px", backgroundColor: "skyblue" }}
+    ></div>
+  );
+};
