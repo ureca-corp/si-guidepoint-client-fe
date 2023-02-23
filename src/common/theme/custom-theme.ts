0f62963ab58e0746df63048d968b5335c1011d48
ssmv713 <sshim9619@naver.com>
2023-02-23 11:06:32 +0900

    build: theme

---
+++ src/common/theme/custom-theme.ts
@@ -0,0 +1,80 @@
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
+import { useMemo, useState } from "react";
+import { LightColor } from "./colors";
+import { ScreenType } from "./screen";
+import { lightTypographyOptions } from "./typography";
+
+export const lightTheme = createTheme({
+  typography: lightTypographyOptions,
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: ScreenType.sm + 1,
+      md: ScreenType.md + 1,
+      lg: ScreenType.lg + 1,
+      xl: ScreenType.xl + 1,
+    },
+  },
+  shape: {
+    borderRadius: 8,
+  },
+  components: {
+    MuiAppBar: {
+      defaultProps: {
+        color: "transparent",
+        elevation: 0,
+        style: {
+          borderBottom: `1px solid ${LightColor.BorderColor1}`,
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: "none",
+        },
+        sizeLarge: {
+          padding: "12px",
+        },
+      },
+    },
+    MuiCard: {
+      defaultProps: {
+        elevation: 0,
+      },
+      styleOverrides: {
+        root: {
+          border: `1px solid ${LightColor.BorderColor2}`,
+        },
+      },
+    },
+  },
+});
+
+export enum ThemeTypes {
+  Light,
+  Dark,
+}
+
+export const findTheme = (theme: ThemeTypes) => {
+  switch (theme) {
+    default:
+      return responsiveFontSizes(lightTheme);
+  }
+};
+
+export const useThemeHook = () => {
+  const [theme, setTheme] = useState(findTheme(ThemeTypes.Light));
+  const palette = theme.palette;
+
+  const colorMode = useMemo(
+    () => (theme: ThemeTypes) => setTheme(findTheme(theme)),
+    []
+  );
+
+  return {
+    theme,
+    palette,
+    colorMode,
+  };
+};
