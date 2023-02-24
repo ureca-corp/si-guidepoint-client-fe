0636c5f152851d4fca0d6b6ac93d57628efa4754
schatzen <gomieti@naver.com>
2023-02-24 21:42:52 +0900

    Add: 링크 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
+import { Link, Stack } from "@mui/material";
 import { NextPage } from "next";
 
 const BusinessPage: NextPage = () => {
-  return <div>Home</div>;
+  return (
+    <Stack paddingY="40px" spacing="20px">
+      <Link href="/login">로그인</Link>
+      <Link href="/signup">회원가입</Link>
+    </Stack>
+  );
 };
 
 export default BusinessPage;
