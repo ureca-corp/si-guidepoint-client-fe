f03be84223b7351a137de661827dff30ce263b22
schatzen <gomieti@naver.com>
2023-03-08 17:30:44 +0900

    Update useFetchTerms.ts

--- src/infra/terms/hooks/useFetchTerms.ts
+++ src/infra/terms/hooks/useFetchTerms.ts
@@ -11,37 +11,30 @@ export const useFetchTerms = (p?: props) => {
   const size = p?.size ?? 10;
 
   const FETCH_TERMS = gql`
-  query {
-    terms(
-        params: {
-            pageRequest: {
-                page: 1,
-                size: 10
-            }
-        }
-    ) {
+    query {
+      terms(params: { pageRequest: { page: 1, size: 10 } }) {
         metaData {
-            offset
-            pageSize
-            pageNumber
-            totalPageCount
-            itemCount
-            totalItemCount
-            isFirst
-            isLast
+          offset
+          pageSize
+          pageNumber
+          totalPageCount
+          itemCount
+          totalItemCount
+          isFirst
+          isLast
         }
         items {
-            id
-            title
-            content
-            isRequired
-            createdAt
-            updatedAt
-            deletedAt
+          id
+          title
+          content
+          isRequired
+          createdAt
+          updatedAt
+          deletedAt
         }
+      }
     }
-}
-`;
+  `;
 
   return useQuery<any>(FETCH_TERMS);
 };
