afa7e4fbe02ae7aa0364337bd050427db209a80d
schatzen <gomieti@naver.com>
2023-03-09 15:55:01 +0900

    Add: circular progress 생성

---
+++ src/common/components/progress/CustomCircularProgress.tsx
@@ -0,0 +1,6 @@
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+
+export const CustomCircularProgress = () => {
+  return <CircularProgress />;
+};
