16a076c92a73899dd4ebcc9259aa9ad8972c8035
schatzen <gomieti@naver.com>
2023-03-09 15:55:37 +0900

    Feat: 회원가입 이메일,패스워드 로직 및 회원 추가 api 연동

--- src/domain/Auth/login/LoginView.tsx
+++ src/domain/Auth/login/LoginView.tsx
@@ -72,6 +72,8 @@ export const LoginView = () => {
 
 const sx = {
   root: css`
+    width: 100%;
+    height: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
--- src/domain/Auth/signup/MemberSignupView.tsx
+++ src/domain/Auth/signup/MemberSignupView.tsx
@@ -1,3 +1,5 @@
+import { BasicDialog } from "@/common/components/dialog/BasicDialog";
+import { CustomCircularProgress } from "@/common/components/progress";
 import { LightColor } from "@/common/theme/colors";
 import { MediaQueries } from "@/common/theme/screen";
 import { css } from "@emotion/react";
@@ -9,10 +11,11 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import { useSignupView } from "./useSignupView";
+import { useMemberSignupView } from "./useMemberSignupView";
 
 export const MemberSignupView = () => {
-  const { email, pw, confirmPw, button } = useSignupView();
+  const { email, pw, confirmPw, button, fetchState } = useMemberSignupView();
+
   return (
     <div css={sx.root}>
       <Card css={sx.inner}>
@@ -25,10 +28,16 @@ export const MemberSignupView = () => {
           </Typography>
         </div>
         <TextField
+          label="이메일 입력"
+          css={sx.input}
           value={email.value}
           onChange={(e) => email.onChange(e.target.value)}
-          css={sx.input}
-          label="이메일 입력"
+          error={email.value !== "" && email.isError}
+          helperText={
+            email.value !== "" &&
+            email.isError &&
+            "이메일 형식이 맞지 않습니다."
+          }
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
@@ -39,10 +48,16 @@ export const MemberSignupView = () => {
         />
         <TextField
           type="password"
+          label="비밀번호 입력"
+          css={sx.input}
           value={pw.value}
           onChange={(e) => pw.onChange(e.target.value)}
-          css={sx.input}
-          label="비밀번호 입력"
+          error={pw.value !== "" && pw.isError}
+          helperText={
+            pw.value !== "" &&
+            pw.isError &&
+            "영문자로 시작하여 특수문자를 포함한 8-20자 여야 합니다."
+          }
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
@@ -53,10 +68,10 @@ export const MemberSignupView = () => {
         />
         <TextField
           type="password"
+          label="비밀번호 확인"
+          css={sx.input}
           value={confirmPw.value}
           onChange={(e) => confirmPw.onChange(e.target.value)}
-          css={sx.input}
-          label="비밀번호 확인"
           error={confirmPw.isError}
           helperText={confirmPw.isError && "비밀번호가 일치하지 않습니다."}
           InputProps={{
@@ -68,19 +83,23 @@ export const MemberSignupView = () => {
           }}
         />
         <Button
-          disabled={!button.isNotDisabled}
-          css={sx.button}
           variant="contained"
+          css={sx.button}
+          disabled={!button.isNotDisabled || fetchState.loading}
+          onClick={button.onSubmit}
         >
-          LOGIN
+          {"SIGN UP"}
         </Button>
       </Card>
+      <BasicDialog />
     </div>
   );
 };
 
 const sx = {
   root: css`
+    width: 100%;
+    height: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
