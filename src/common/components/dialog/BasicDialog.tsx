1bab084d1804b47eb8ee693cb2d6bd3a741cca29
schatzen <gomieti@naver.com>
2023-03-09 15:54:44 +0900

    Add: 전역적 dialog 컴포넌트 및 recoil. state 생성

---
+++ src/common/components/dialog/BasicDialog.tsx
@@ -0,0 +1,31 @@
+import { css } from "@emotion/react";
+import { UseDialogOpen } from "@/common/hooks/UseDialogOpen";
+import { Button, Dialog, DialogTitle, Stack } from "@mui/material";
+import { useRouter } from "next/router";
+
+export const BasicDialog = () => {
+  const router = useRouter();
+  const { openState, msgState, hrefState } = UseDialogOpen();
+  return (
+    <Dialog onClose={() => openState.onClose()} open={openState.value}>
+      <Stack css={sx.inner}>
+        <DialogTitle variant="subtitle1">{msgState.value}</DialogTitle>
+        <Button
+          onClick={
+            hrefState.value == ""
+              ? () => openState.onClose()
+              : () => router.push(hrefState.value)
+          }
+        >
+          {"확인"}
+        </Button>
+      </Stack>
+    </Dialog>
+  );
+};
+
+const sx = {
+  inner: css`
+    width: 100%;
+  `,
+};
---
+++ src/common/hooks/UseDialogOpen.ts
@@ -0,0 +1,46 @@
+import { atom, useRecoilState } from "recoil";
+
+const GlobalDialogOpenState = atom({
+  key: "globalDialogOpenState",
+  default: {
+    isOpen: false,
+    msg: "",
+    href: "",
+  },
+});
+
+export const UseDialogOpen = () => {
+  const [dialogState, setDialogState] = useRecoilState(GlobalDialogOpenState);
+
+  const onErrorOpen = (msg: string) => {
+    setDialogState({ isOpen: true, msg: msg, href: "" });
+  };
+
+  const onComfirmOpen = (msg: string, href?: string) => {
+    setDialogState((old) => ({
+      ...old,
+      isOpen: true,
+      msg: msg,
+      href: href ?? "",
+    }));
+  };
+
+  const onClose = () => {
+    setDialogState((old) => ({ ...old, isOpen: false }));
+  };
+
+  return {
+    openState: {
+      value: dialogState.isOpen,
+      onClose: onClose,
+      onErrorOpen: onErrorOpen,
+      onConfirmOpen: onComfirmOpen,
+    },
+    msgState: {
+      value: dialogState.msg,
+    },
+    hrefState: {
+      value: dialogState.href,
+    },
+  };
+};
