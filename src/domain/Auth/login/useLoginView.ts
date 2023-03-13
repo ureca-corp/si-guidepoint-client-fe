8cdc76f0a1d39a25962a65e5d3a9e8051f8e3e30
schatzen <gomieti@naver.com>
2023-03-13 19:52:07 +0900

    Feat: 로그인 성공 시 user-session 저장

--- src/domain/Auth/login/useLoginView.ts
+++ src/domain/Auth/login/useLoginView.ts
@@ -1,14 +1,29 @@
+import { useSessionUser } from "@/common/auth/session/application";
+import { UserRouterPath } from "@/common/router/RouterPath";
 import { useMemberLogin } from "@/infra/member/hooks/useMemberLogin";
 import { emailRegex } from "@/utils/Regex";
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useCallback, useEffect, useState } from "react";
 
 export const useLoginView = () => {
+  // router
+  const router = useRouter();
+  const routeHome = () => router.replace(UserRouterPath.Profile);
+
+  //session
+  const { setUser, removeUser } = useSessionUser();
+
+  // login 진입시 세션제거
+  useEffect(() => {
+    removeUser();
+  }, []);
+
   // state
   const [email, setEmail] = useState("");
   const [pw, setPw] = useState("");
 
   // api
-  const { login, loading } = useMemberLogin();
+  const { login, loading, data } = useMemberLogin();
 
   // functions
   const handleEmailChange = (value: string) => {
@@ -18,13 +33,28 @@ export const useLoginView = () => {
     setPw(value);
   };
 
+  // error state
+  const isEmailRegexRight = emailRegex.test(email);
+  const isFilled = email !== "" && pw !== "" && isEmailRegexRight;
+
+  // login
   const onSubmit = () => {
     login({ variables: { email: email, password: pw } });
   };
 
-  // error state
-  const isEmailRegexRight = emailRegex.test(email);
-  const isFilled = email !== "" && pw !== "" && isEmailRegexRight;
+  // when login successed
+  const handleLoginSuccess = useCallback(() => {
+    if (data) {
+      const { user, accessToken } = data.login;
+      setUser({ userId: user.userId, email: user.email, accessToken });
+
+      routeHome();
+    }
+  }, [data]);
+
+  useEffect(() => {
+    handleLoginSuccess();
+  }, [handleLoginSuccess]);
 
   return {
     email: {
